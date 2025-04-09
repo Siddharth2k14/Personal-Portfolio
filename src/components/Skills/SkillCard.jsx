d52220f61b70b29c7a96788c64ed1ba06af712ff
Siddharth <siddharthsingh2k@gmail.com>
2025-04-10 00:40:10 +0530

    Updating SkillCrad.jsx

--- src/components/Skills/SkillCard.jsx
+++ src/components/Skills/SkillCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function SkillCard({ title, description, link }) {
   const content = (
-    <h3 className="text-2xl mb-2.5 text-[#39FF14]">{title}</h3>
+    <h3 className="text-2xl mb-2.5 font-bold text-[#39FF14]">{title}</h3>
   );
 
   return (
-    <div className="w-48 p-5 bg-[url('https://images.unsplash.com/photo-1451187580459-43490279c0fa')] bg-center rounded-lg shadow-md transition-transform duration-300 hover:-translate-y-1.5">
+    <div className="w-48 p-5 bg-center rounded-lg shadow-md transition-transform duration-300 hover:-translate-y-1.5">
       {link ? (
         <a href={link} className="text-[#39FF14] hover:underline">
           {content}
@@ -14,7 +14,7 @@ export function SkillCard({ title, description, link }) {
       ) : (
         content
       )}
-      <p className="text-base text-gray-100">{description}</p>
+      <p className="text-base text-gray-100 italic font-serif">{description}</p>
     </div>
   );
 }
