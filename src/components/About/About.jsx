6d79e36920766395c444e6c7bddf0673a10bf751
Siddharth <siddharthsingh2k@gmail.com>
2025-04-10 00:02:17 +0530

    Checking

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <div className="m-2.5 border-2 border-gray-100 bg-[url('/Background.png')] bg-fixed animate-gradient">
       <section id="about" className="p-10 text-center">
-        <h2 className="text-3xl mb-5 text-gray-100">About Me</h2>
+        <h2 className="text-3xl mb-5 text-gray-100">About M</h2>
         <h2 className="text-3xl mb-5 text-gray-100">Hi, I'm Siddharth Singh</h2>
         <p className="text-gray-100">Web Developer | Designer | Problem Solver</p>
         <Button>View My Work</Button>
